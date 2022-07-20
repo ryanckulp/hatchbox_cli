08c1a5ff3be2947c869129d07723d6570e714893
Ryan Kulp <ryanckulp@gmail.com>
2022-07-20 19:14:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HatchBox CLI
+Unofficial bash script that makes SSH-ing into your server a bit easier.
+
+## Installation
+on Mac:
+```sh
+echo curl https://raw.githubusercontent.com/ryanckulp/hatchbox_cli/script.sh >> ~/.bash_profile
+```
+
---
+++ script.sh
@@ -0,0 +1,27 @@
+function hb() {
+  user="deploy"
+  address="xxx.xxx.xxx.xxx" # REPLACE ME
+
+  if [[ $# < 2 ]]; then
+    echo "HatchBox CLI: Must provide an app slug and command, e.g hb <app> <command>"
+    echo ""
+    echo "Available commands:"
+    echo "hb <app> console         Run rails console for <app>"
+    echo "hb <app> logs            Read live logs for <app>"
+    return
+  fi
+
+  app=${1}
+  action=${2}
+
+  if [[ ${action} == 'logs' ]]; then
+    echo "tailing the ${app} logs..."
+    action="journalctl --user --unit=${app}-server" # HatchBox v2.0
+    # action="tail -f ~/${app}/current/log/production.log" # enable for HatchBox Classic
+  elif [[ ${action} == 'console' ]]; then
+    echo "opening the ${app} console..."
+    action="cd ~/${app}/current && bundle exec rails console"
+  fi
+
+  ssh -t ${user}@${address} ${action}
+}
